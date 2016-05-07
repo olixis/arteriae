6c1251781e0415af3ab8193b3b7a7c08563bbfd5
Caio Fontes <olixis@gmail.com>
2016-05-07 19:13:43 -0300

    search  keyword by get param

--- index.js
+++ index.js
@@ -2,27 +2,56 @@ var querystring = require('querystring');
 var request = require('request');
 var cheerio = require('cheerio');
 var google = require('google');
+var restify = require('restify');
+var emitter = require('events'),
+    eventEmitter = new emitter.EventEmitter();
 var MAXRESULTS = 10;
+var server = restify.createServer();
+server.use(restify.queryParser());
 
-module.exports.feed = function headLinesBySite(theme, word) {
+module.exports.feedServer = function() {
+    server.get('/:keyword', function(req, res, next) {
+        console.log(req.params);
+        feed(req.params.keyword);
+        eventEmitter.once('retorno', function(retorno) {
+            res.send(retorno);
+            return next();
+        });
+    });
+    server.on("listening", function() {
+        console.log("server running!");
+    });
+    server.listen(7171);
+};
+
+var feed = function headLinesBySite(word, l) {
+    if (!Array.isArray(l) && l!== undefined) {
+        console.log("por favor passe um array");
+        return false;
+    }
+    if (l !== []) {
+        links = l;
+    }
+    links = ["http://www.globo.com/", "http://atarde.uol.com.br/"];
     var teste = 0;
     var final = [];
-    findAllGoogleURLs(theme, function(links) {
-        for (i = links.length - 1; i >= 0; i--) {
-            findHeadlines(links[i], word, function(r, url) {
-                final.push({
-                    url: url,
-                    manchetes: r
-                });
-                //console.log(url+" - "+r);
-                teste++;
-                if (teste === MAXRESULTS - 1) {
-                    console.log(final);
-                    return final;
-                }
+    for (i = links.length - 1; i >= 0; i--) {
+        findHeadlines(links[i], word.toLowerCase(), function(r, url) {
+            final.push({
+                url: url,
+                keyword: word,
+                manchetes: r
             });
-        }
-    });
+            //console.log(url+" - "+r);
+            teste++;
+            console.log((teste / links.length) * 100 + "%");
+            if (teste === links.length) {
+                console.log("Concluído!");
+                console.log(final);
+                eventEmitter.emit('retorno', final);
+            }
+        });
+    }
 };
 
 var findHeadlines = function findHeadlines(url, word, cb) {
@@ -33,16 +62,18 @@ var findHeadlines = function findHeadlines(url, word, cb) {
             $ = cheerio.load(body);
             texto = [];
             var retorno = [];
-            b = $('body').text().toLowerCase();
+            b = $('a').text().toLowerCase();
+            //console.log(b);
             c = b.replace(/\s+/g, ' ');
             texto = c.split("¬");
             for (var i = texto.length - 1; i >= 0; i--) {
-                if (texto[i].search(word) !== -1 && texto[i].length < 100) {
+                //if (texto[i].search(word) !== -1 && texto[i].length < 100) {
                     //console.log(texto[i]);
+                    if(texto[i]!== "" && texto[i] !== " "){
                     retorno.push("+" + texto[i] + "+");
                 }
+                //}
             }
-            //console.log(retorno);
             cb(retorno, url);
         } else {
             console.log(error);
@@ -50,55 +81,4 @@ var findHeadlines = function findHeadlines(url, word, cb) {
         }
     });
 };
-var test = function() {
-    google.resultsPerPage = 11;
-    var nextCounter = 0;
-    google('lasanha', function(err, res) {
-        if (err) console.error(err);
-        for (var i = 0; i < res.links.length; ++i) {
-            var link = res.links[i];
-            console.log(link.href);
-        }
-    });
-};
-var findAllGoogleURLs = function(theme, cb) {
-    request({
-        url: "https://www.google.com/search?hl=pt-BR&q=" + theme + "&start=0&sa=N&num=" + MAXRESULTS + "&ie=UTF-8&oe=UTF-8&gws_rd=ssl",
-        method: 'GET'
-    }, function(error, response, body) {
-        var links = [];
-        if (!error && response.statusCode == 200) {
-            $ = cheerio.load(body);
-            var a = $("div.g");
-            a.each(function(i, elem) {
-                linkElem = $(elem).find('h3.r a');
-                var qsObj = querystring.parse($(linkElem).attr('href'));
-                if (qsObj['/url?q']) {
-                    links.push(qsObj['/url?q']);
-                }
-            });
-            //console.log(links);
-        } else {
-            console.log(error);
-        }
-        cb(links);
-    });
-};
 
-function findAllURLs(url) {
-    request(url, function(error, response, body) {
-        var links = [];
-        if (!error && response.statusCode == 200) {
-            $ = cheerio.load(body);
-            $("a").each(function() {
-                if ($(this).attr('href') !== undefined && $(this).attr('href') !== "") {
-                    links.push($(this).attr('href'));
-                }
-            });
-            console.log(links);
-        } else {
-            console.log(error);
-        }
-        return links;
-    });
-}
